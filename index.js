bcef1577e46af02c861e9f1af470fff5c01e9e77
Minhajmurad <minhajmurad421@gmail.com>
2022-11-24 13:26:42 +0600

    server for catagories done

--- index.js
+++ index.js
@@ -9,10 +9,21 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+const bikecategories = require('./data/catagories.json');
+
 app.get('/', (req, res) => {
     res.send('Bikers Avenue API Running');
 });
 
+app.get('/bikecategories', (req, res) => {
+    res.send(bikecategories)
+});
+
+app.get('/bikecategories/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedcatgories = bikecategories.find(n => n.id === id);
+    res.send(selectedcatgories);
+});
 app.listen(port, ()=>{
     console.log(`Bikers Avenue is running on port ${port}`);
 })
